72fef28738884d0dcf5d85676c1f17260fb04c42
Christofer Gomez <christofergg@gmail.com>
2025-02-10 16:37:11 +0100

    03-functions definicion de funciones y lambdas, uso de argumentos opcionales y con valor por defecto

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 // import './topics/01-basic-types.ts'
- import './topics/02-object-interface'
+//  import './topics/02-object-interface'
+import './topics/03-functions'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola Mundo!
