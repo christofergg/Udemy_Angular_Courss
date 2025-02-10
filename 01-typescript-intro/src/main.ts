66dd42cfcf9bab3020ac62fedeea53121a48df8f
Christofer Gomez <christofergg@gmail.com>
2025-02-10 17:32:37 +0100

    04-homework-types se ha facilitado un ejercicio con un objecto se debe definir la interfaz.

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 // import './topics/01-basic-types.ts'
 //  import './topics/02-object-interface'
-import './topics/03-functions'
+// import './topics/03-functions'
+import './topics/04-homewokr-types'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola Mundo!
