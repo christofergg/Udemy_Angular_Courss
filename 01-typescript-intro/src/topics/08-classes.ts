9e6ea6304f754d685ed6f5a51a0b7b6227444bc9
Christofer Gomez <christofergg@gmail.com>
2025-02-11 09:03:29 +0100

    Herencia vs composicion

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -2,7 +2,8 @@ export class Person {
     public phone? : string;
 
     constructor(
-        public name : string, 
+        public firstName : string, 
+        public lastName : string, 
         private address : string = 'Sin dirección') {
     }
 }
@@ -17,7 +18,22 @@ export class Hero extends Person {
         }
 }
 
-const ironMan: Person = new Hero('IronMan', 45, 'Tony Stark');
-ironMan.phone = '656666666';
+//se debe priorizar la composición del objeto sobre el nivel de Herencia. por eso en vez de heredar 
+//se va a pasar por referencia el objeto. de esta forma la clase NewHero no se ve afectada por los posibles
+//cambios de la clase persona.
+
+export class NewHero {
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+        public person: Person
+    ) {
+
+    }
+}
+
+const tony: Person = new Person('Tony', 'Stark', 'New York');
+const ironMan: NewHero = new NewHero('IronMan', 45, 'Tony Stark', tony);
 
 console.log(ironMan);
