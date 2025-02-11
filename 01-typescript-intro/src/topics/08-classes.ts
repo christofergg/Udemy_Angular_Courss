c18e678e33fa225db165a8a19674bbbad61d0ade
Christofer Gomez <christofergg@gmail.com>
2025-02-11 08:49:01 +0100

    customización del constructor

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -1,15 +1,13 @@
 export class Person {
-    public name : string;
-    private address : string;
     public phone? : string;
 
-    constructor(name : string, address : string) {
-        this.name = name;
-        this.address = address;
+    constructor(
+        public name : string, 
+        private address : string) {
     }
 }
 
 const ironMan: Person = new Person('Christofer', 'Calle False 123');
 ironMan.phone = '656666666';
 
-console.log(ironMan.name)
+console.log(ironMan);
