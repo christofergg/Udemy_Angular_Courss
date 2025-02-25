c5a4bbe603225e872a0c3665fc49693c537b2608
Christofer Gomez <christofergg@gmail.com>
2025-02-25 23:26:24 +0100

    kill port 4200 and for

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+
+interface Character {
+  id: number,
+  name: string,
+  power: number
+}
 
 @Component({
   selector: 'app-dragonball-page.component',
@@ -7,5 +14,16 @@ import { Component } from '@angular/core';
   styleUrl: './dragonball-page.component.css'
 })
 export class DragonballPageComponentComponent {
+  characters = signal<Character[]>([
+    {
+      id : 1, name: 'Goku', power: 9001
+    },
+    {
+      id : 1, name: 'Vegeta', power: 8000
+    },
+    {
+      id : 1, name: 'Piccolo', power: 3000
+    },
+]);
 
 }
